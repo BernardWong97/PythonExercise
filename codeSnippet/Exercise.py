d0607564bd3f8d671a4dde8acd57593c5c169a42
Bernard Wong <ben.wc88@gmail.com>
2017-11-16 01:02:35 +0000

    Create repo initial commit
    Create a repo holds the progress of my learning of Python

---
+++ codeSnippet/Exercise.py
@@ -0,0 +1,25 @@
+print("this line will be printed")
+
+myInt = 123
+print(myInt)
+myFloat = 123.4
+print(myFloat)
+myString = "Hello World!"
+print(myString)
+myString = 'Hello World!'
+print(myString)
+myString = "Don't worry about apostrophes"
+print(myString)
+
+int1 = 1
+int2 = 2
+int3 = int1 + int2
+print(int3)
+
+st1 = "Hello"
+st2 = "World!"
+st3 = st1 + " " + st2
+print(st3)
+
+a, b = 5, 6
+print(a,b)
